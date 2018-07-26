1010889b5f2621a8e00ef1602cae2b3c37cc6d57
merveer <merveuygunerdol@gmail.com>
2018-07-26 12:47:53 +0300

    Start timer when seat selected

--- checkin/src/components/checkin.vue
+++ checkin/src/components/checkin.vue
@@ -1,36 +1,45 @@
 <template>
-    <div>
-        <b-container>
-            <b-row>
-              <b-col sm>{{msg}}<h1>{{seat}}</h1>
-                Seat Price <h1>{{seatprice}} €</h1>
-                  Check-in Price<h1>{{fixedprice}} €</h1>
-                  Total Price<h1>{{totalprice}} €</h1>
-              </b-col>
-                <span class="rowNumber" v-for="seats in 12" :key="seats">
-                <span class="rowLetter" v-if="seats % 4 !== 0">{{Seats[seats-1]}}</span>
-                    <b-col v-if="seats % 4 === 0">
-                        <span v-if="seats == 12" v-for="n in 21" :key="n">
-                            <span class="rowLetter" v-if="n != 0"  :key="n">
-                        {{n-1}}<br>
-                            </span>
-                        </span>
-                    </b-col>
-                    <b-col sm v-else>
-                        <span v-for="n in 20" :key="n">
-                        <button @click="selectSeat(Seats[seats-1]+n)"></button><br>
-                        </span>
-                    </b-col>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col sm>
+          {{msg}}<h1>{{seat}}</h1>
+          Seat Price <h1>{{seatprice}} €</h1>
+          Check-in Price<h1>{{fixedprice}} €</h1>
+          Total Price<h1>{{totalprice}} €</h1>
+        </b-col>
+        <span class="rowNumber" v-for="seats in 12" :key="seats">
+          <span class="rowLetter" v-if="seats % 4 !== 0">{{Seats[seats-1]}}</span>
+            <b-col v-if="seats % 4 === 0" sm>
+              <span v-if="seats == 12" v-for="n in 21" :key="n">
+                <span class="rowLetter" v-if="n != 0"  :key="n">
+                  {{n-1}}<br>
                 </span>
-                <b-col sm>
-                  <h2>Welcome</h2>
-                  <h3>{{name}}</h3>
-                    <b-button class="gon" variant="success">Check-out</b-button>
-                    <b-button class="gon">Continue without selecting seat</b-button>
-                </b-col>
-            </b-row>
-        </b-container>
-    </div>
+              </span>
+            </b-col>
+            <b-col sm v-else>
+              <span v-for="n in 20" :key="n">
+                <button :disabled="disablebutton" class="button_clicked" @click="selectSeat(Seats[seats-1]+n)"></button><br>
+              </span>
+            </b-col>
+        </span>
+        <b-col sm>
+          <h2>Welcome</h2>
+          <h3>{{name}}</h3>
+            Time Left to Check-out<h3>{{minute}}.{{second}}</h3>
+              <b-button variant="success">Check-out</b-button>
+              <b-button>Continue without selecting seat</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+    <b-modal ref="myModalRef" hide-footer title="Pick This Seat">
+        <div class="d-block text-center">
+          <h3>Are you sure to select {{seat}}?</h3>
+        </div>
+        <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">NO</b-btn>
+        <b-btn z variant="outline-success" block @click="startTimer">YES</b-btn>
+    </b-modal>
+  </div>
 </template>
 
 <script>
@@ -40,21 +49,42 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 export default {
   data () {
     return {
-      Seats: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K'],
+      Seats: ['A', 'B', 'C', ' ', 'D', 'E', 'F', ' ', 'G', 'H', 'I'],
       seat: '',
       msg: 'Select a Seat...',
       fixedprice: 8,
       totalprice: 0,
       seatprice: 0,
       name: '',
-      email: ''
+      email: '',
+      second: 0,
+      minute: 3,
+      disablebutton: false
     }
   },
   created () {
     this.fetchUser()
   },
   methods: {
+    startTimer () {
+      this.disablebutton = true
+      this.$refs.myModalRef.hide()
+      setInterval(this.timer, 1000)
+    },
+    timer () {
+      if (this.second === 0) {
+        this.minute = this.minute - 1
+        this.second = 59
+        if (this.minute === -1) {
+          window.location = 'http://localhost:8080/checkin'
+        }
+      } else {
+        this.second -= 1
+        this.second = this.second
+      }
+    },
     selectSeat (seat) {
+      this.showModal()
       if (seat[0] === 'A' || seat[0] === 'K') {
         this.seatprice = 10
       }
@@ -76,6 +106,12 @@ export default {
         .catch(e => {
           this.errors.push(e)
         })
+    },
+    showModal () {
+      this.$refs.myModalRef.show()
+    },
+    hideModal () {
+      this.$refs.myModalRef.hide()
     }
   }
 }
@@ -90,7 +126,7 @@ export default {
 .gon {
     margin: 1em;
 }
-button:focus {
+.button_clicked:focus {
     background-color: rgb(55, 0, 255);
     height: 1em;
     width: 1em;
