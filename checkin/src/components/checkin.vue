281a7f902a8ebc4cffb72957a8d5f36c01f188a7
merveer <merveuygunerdol@gmail.com>
2018-07-30 13:06:48 +0300

    Refactor for vuex

--- checkin/src/components/checkin.vue
+++ checkin/src/components/checkin.vue
@@ -3,26 +3,16 @@
     <b-container>
       <b-row>
         <b-col sm>
-          {{msg}}<h1>{{seat}}</h1>
+          {{msg}}<h1>{{$store.state.seat}}</h1>
           Seat Price <h1>{{seatPrice}} €</h1>
           Check-in Price<h1>{{fixedPrice}} €</h1>
           Total Price<h1>{{totalPrice}} €</h1>
         </b-col>
-        <span class="rowNumber" v-for="seats in 12" :key="seats">
-          <span class="rowLetter" v-if="seats % 4 !== 0">{{Seats[seats-1]}}</span>
-            <b-col v-if="seats % 4 == 0" sm>
-              <span v-if="seats == 12" v-for="seat in 21" :key="seat">
-                <span class="rowLetter" v-if="seat != 0"  :key="seat">
-                  {{seat-1}}<br>
-                </span>
-              </span>
-            </b-col>
-            <b-col sm v-else>
-              <span v-for="seat in 20" :key="seat">
-                <button v-if="clickedButton == Seats[seats-1]+seat" v-bind:class="{button_clicked: isActive}" :disabled="disablebutton" class="button" @click="selectSeat(Seats[seats-1]+seat)"></button>
-                <button v-else :disabled="disablebutton" class="button" @click="selectSeat(Seats[seats-1]+seat)"></button><br>
-              </span>
-            </b-col>
+        <span class="rowNumber" v-for="(seats, row) in seatsList" :key="row">
+          {{row}}<br>
+          <span class="rowNumber" v-for="seat in seats" :key="seat">
+            <button :disabled="disablebutton" @click="selectSeat(seat)">{{seat[1]}}</button><br>
+            </span>
         </span>
         <b-col sm>
           <h2>Welcome</h2>
@@ -35,7 +25,7 @@
     </b-container>
     <b-modal ref="myModalRef" hide-footer title="Pick This Seat">
         <div class="d-block text-center">
-          <h3>Are you sure to select {{seat}}?</h3>
+          <h3>Are you sure to select {{$store.state.seat}}?</h3>
         </div>
         <b-btn class="mt-3" variant="outline-danger" block @click="hideModal">NO</b-btn>
         <b-btn z variant="outline-success" block @click="startTimer">YES</b-btn>
@@ -69,8 +59,8 @@ export default {
     ...mapState([
       'msg',
       'fixedPrice',
-      'Seats',
-      'seat',
+      'seatsList',
+      'seats',
       'seatPrice',
       'totalPrice',
       'clickedButton',
@@ -118,15 +108,14 @@ export default {
         this.second = 0
         this.minute = 3
         this.isActive = true
-        window.location.reload()
       }
     },
     random () {
-      let ran1 = Math.floor(Math.random() * (7 - 1 + 1)) + 1
-      let ran2 = Math.floor(Math.random() * (20 - 1 + 1)) + 1
-      let randomNums = [ran1, ran2]
-      if (randomNums[0] !== 3 && randomNums[1] !== 7) {
-        this.$store.dispatch('changeData', randomNums)
+      this.$store.dispatch('randomNumbers')
+      console.log(this.$store.state.randomNums[1])
+      console.log(this.$store.state.randomNums[0])
+      if (this.$store.state.randomNums[0] != 3 && this.$store.state.randomNums[0] != 7) {
+        this.$store.dispatch('randomSeat')
         this.startTimer()
       }
     },
@@ -134,13 +123,16 @@ export default {
       this.$refs.myModalRef.show()
     },
     hideModal () {
-      this.$store.state.isActive = false
-      this.$store.state.seatPrice = 0
-      this.$store.state.fixedPrice = 8
-      this.$store.state.totalPrice = 0
-      this.$store.state.msg = 'Select a Seat...'
-      this.$store.state.seat = ''
-      this.$store.state.activeModal = false
+      let data = {
+        isActive: false,
+        seatPrice: 0,
+        fixedPrice: 8,
+        totalPrice: 0,
+        msg: 'Select a Seat...',
+        seat: '',
+        activeModal: false
+      }
+      this.$store.dispatch('changeData', data)
       this.$refs.myModalRef.hide()
     }
   }
--- checkin/src/store/index.js
+++ checkin/src/store/index.js
@@ -1,51 +1,103 @@
 import axios from 'axios'
-
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import checkin from '../components/checkin'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: [
+    {
+      path: '/checkin',
+      name: 'checkin',
+      component: checkin
+    }
+  ]
+})
 const state = {
   msg: 'Select a Seat...',
   fixedPrice: 8,
-  Seats: ['A', 'B', 'C', ' ', 'D', 'E', 'F', ' ', 'G', 'H', 'I'],
-  seat: '',
+  seatsList: {
+    'A': ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'A11', 'A12', 'A13', 'A14', 'A15', 'A16', 'A17', 'A18', 'A19', 'A20'],
+    'B': ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B13', 'B14', 'B15', 'B16', 'B17', 'B18', 'B19', 'B20'],
+    'C': ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'C11', 'C12', 'C13', 'C14', 'C15', 'C16', 'C17', 'C18', 'C19', 'C20'],
+    'D': ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'D11', 'D12', 'D13', 'D14', 'D15', 'D16', 'D17', 'D18', 'D19', 'D20'],
+    'E': ['E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'E11', 'E12', 'E13', 'E14', 'E15', 'E16', 'E17', 'E18', 'E19', 'E20'],
+    'F': ['F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'F20'],
+    'G': ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 'G14', 'G15', 'G16', 'G17', 'G18', 'G19', 'G20'],
+    'H': ['H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20'],
+    'I': ['I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'I11', 'I12', 'I13', 'I14', 'I15', 'I16', 'I17', 'I18', 'I19', 'I20']
+  },
+  rows: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
   seatPrice: 0,
   totalPrice: 0,
   clickedButton: '',
   isActive: false,
   activeModal: false,
-  user: ''
+  user: '',
+  randomNums: []
 }
 
 const getters = {}
 
 const mutations = {
-  pickSeat (state, selectedSeat) {
+  SET_SEAT (state, selectedSeat) {
     state.seat = selectedSeat
   },
   SET_USER (state, user) {
     state.user = user
+  },
+  SET_SEATPRICE (state, seatp) {
+    state.seatPrice = seatp
+  },
+  SET_FIXEDPRICE (state, fixedp) {
+    state.fixedPrice = fixedp
+  },
+  SET_TOTALPRICE (state, totalp) {
+    state.totalPrice = totalp
+  },
+  SET_MSG (state, msg) {
+    state.msg = msg
+  },
+  SET_MODAL (state, status) {
+    state.activeModal = status
+  },
+  SET_RANDOMSEAT (state, seat) {
+    state.seat = seat
+    state.fixedPrice = 0
+    state.seatPrice = 0
+    state.totalPrice = 0
+    state.msg = 'Selected Seat:'
+  },
+  SET_ACTIVITY (state, status) {
+    state.isActive = status
+  },
+  SET_RANDOMNUMS (state, nums) {
+    state.randomNums = [nums[0], nums[1]]
+  },
+  SET_BUTTON (state, button) {
+    state.clickedButton = button
   }
 }
 const actions = {
   selectSeat ({commit}, selectedSeat) {
-    commit('pickSeat', selectedSeat)
-    state.clickedButton = state.seat
-    state.isActive = true
-    if (state.seat[0] == 'A' || state.seat[0] == 'I') {
-      state.seatPrice = 10
+    commit('SET_ACTIVITY', true)
+    commit('SET_BUTTON', selectedSeat)
+    if (selectedSeat[0] == 'A' || selectedSeat[0] == 'I') {
+      commit('SET_SEATPRICE', 10)
     }
-    if (state.seat[0] == 'C' || state.seat[0] == 'D' || state.seat[0] == 'F' || state.seat[0] == 'G') {
-      state.seatPrice = 8
+    if (selectedSeat[0] == 'C' || selectedSeat[0] == 'D' || selectedSeat[0] == 'F' || selectedSeat[0] == 'G') {
+      commit('SET_SEATPRICE', 8)
     }
-    if (state.seat[0] == 'B' || state.seat[0] == 'E' || state.seat[0] == 'H') {
-      state.seatPrice = 5
+    if (selectedSeat[0] == 'B' || selectedSeat[0] == 'E' || selectedSeat[0] == 'H') {
+      commit('SET_SEATPRICE', 5)
     }
-    if (state.seat[2]) {
-      state.seat = state.seat[0] + '-' + state.seat[1] + state.seat[2]
+    if (selectedSeat[2]) {
+      commit('SET_SEAT', selectedSeat[0] + '-' + selectedSeat[1] + selectedSeat[2])
     } else {
-      state.seat = state.seat[0] + '-' + state.seat[1]
+      commit('SET_SEAT', selectedSeat[0] + '-' + selectedSeat[1])
     }
-    state.totalPrice = state.seatPrice + state.fixedPrice
-    state.msg = 'Selected Seat:'
-    console.log(selectedSeat)
-    state.activeModal = true
+    commit('SET_TOTALPRICE', state.seatPrice + state.fixedPrice)
+    commit('SET_MSG', 'Selected Seat:')
+    commit('SET_MODAL', true)
   },
   fetchUser ({ commit }) {
     axios.get('http://localhost:3030/user/fetch')
@@ -62,20 +114,32 @@ const actions = {
     axios.post('http://localhost:3030/user/add', newUser)
       .then((response) => {
         console.log(response.data)
-        window.location = 'http://localhost:8080/#/checkin'
+        router.push('checkin')
+        router.go('checkin')
+        commit('SET_ACTIVITY', false)
       })
       .catch((error) => {
         console.log(error)
       })
   },
-  changeData ({ commit }, randomNums) {
-    console.log(randomNums)
-    state.seat = state.Seats[randomNums[0]] + randomNums[1]
-    state.fixedPrice = 0
-    state.seatPrice = 0
-    state.totalPrice = 0
-    state.msg = 'Selected Seat:'
+  randomNumbers ({ commit }) {
+    let ran1 = Math.floor(Math.random() * (7 - 1 + 1)) + 1
+    let ran2 = Math.floor(Math.random() * (20 - 1 + 1)) + 1
+    commit('SET_RANDOMNUMS', [ran1, ran2])
+  },
+  randomSeat ({ commit }, randomNums) {
+    let letter = state.rows[state.randomNums[0]]
+    commit('SET_RANDOMSEAT', letter + '-' + state.randomNums[1])
     this.disablebutton = true
+  },
+  changeData ({ commit }, data) {
+    commit('SET_ACTIVITY', data.isActive)
+    commit('SET_SEATPRICE', data.seatPrice)
+    commit('SET_FIXEDPRICE', data.fixedPrice)
+    commit('SET_TOTALPRICE', data.totalPrice)
+    commit('SET_MSG', data.msg)
+    commit('SET_SEAT', data.seat)
+    commit('SET_MODAL', data.activeModal)
   }
 }
 
